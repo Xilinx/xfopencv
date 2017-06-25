358d55ee77df0ca277556a72da5e45a983183464
Abhishek Yenamandra <abhishek.yenamandra@xilinx.com>
2017-06-25 11:58:03 +0530

    Added checks to image reads

--- examples/stereolbm/xf_stereoBM_tb.cpp
+++ examples/stereolbm/xf_stereoBM_tb.cpp
@@ -45,7 +45,17 @@ int main(int argc, char** argv)
 	cv::Mat left_img, right_img;
 
 	left_img = cv::imread(argv[1],0);
+	if(!left_img.data)
+	{
+		printf("Failed to load left image ... %s\n!", argv[1]);
+		return -1;
+	}
 	right_img = cv::imread(argv[2],0);
+	if(!right_img.data)
+	{
+		printf("Failed to load right image ... %s\n!", argv[2]);
+		return -1;
+	}
 
 	cv::Mat disp;
 
