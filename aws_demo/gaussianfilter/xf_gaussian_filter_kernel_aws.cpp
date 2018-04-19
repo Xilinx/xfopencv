102ce408d0b29da1a8db9c60022f3fd6efc27e8a
Serge <sefo@ru.ru>
2018-04-19 04:48:55 +0300

    Add resizing of  filtered image (host sources interface didn't changed and become incompatible with kernel)

--- aws_demo/gaussianfilter/xf_gaussian_filter_kernel_aws.cpp
+++ aws_demo/gaussianfilter/xf_gaussian_filter_kernel_aws.cpp
@@ -3,59 +3,88 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "hls_stream.h"
 
 #include "common/xf_common.h"
 
 #include "xf_gaussian_filter_config.h"
 
 #include "imgproc/xf_gaussian_filter.hpp"
+#include "imgproc/xf_resize.hpp"
+
+#define SRC_T       XF_8UC1
+
+#define INTERPOLATION_TYPE XF_INTERPOLATION_NN
+
+extern "C" void xf_gaussian_filter(XF_TNAME(SRC_T,NPC1) *ai, XF_TNAME(SRC_T,NPC1) *bo, int rows, int cols, float sigma, int o_rows, int o_cols);
 
 
-extern "C" void xf_gaussian_filter(unsigned int *a, unsigned int *c, int rows, int cols, float sigma);
-
-
-    void xf_gaussian_filter(unsigned int *a, unsigned int *c, int rows, int cols, float sigma)
+    void xf_gaussian_filter(XF_TNAME(SRC_T,NPC1) *ai, XF_TNAME(SRC_T,NPC1) *bo, int rows, int cols, float sigma, int o_rows, int o_cols)
     {
-      #pragma HLS INTERFACE m_axi     port=a  offset=slave bundle=gmem
-      #pragma HLS INTERFACE m_axi     port=c  offset=slave bundle=gmem
-      #pragma HLS INTERFACE s_axilite port=a               bundle=control
-      #pragma HLS INTERFACE s_axilite port=c               bundle=control
-      #pragma HLS INTERFACE s_axilite port=rows            bundle=control
-      #pragma HLS INTERFACE s_axilite port=cols            bundle=control
-      #pragma HLS INTERFACE s_axilite port=sigma           bundle=control
-      #pragma HLS INTERFACE s_axilite port=return          bundle=control
+      #pragma HLS INTERFACE m_axi     port=ai  offset=slave bundle=gmem
+      #pragma HLS INTERFACE m_axi     port=bo  offset=slave bundle=gmem
 
-      #pragma HLS inline region
+      #pragma HLS INTERFACE s_axilite port=ai               bundle=control
+      #pragma HLS INTERFACE s_axilite port=bo               bundle=control
+      
+      #pragma HLS INTERFACE s_axilite port=rows             bundle=control
+      #pragma HLS INTERFACE s_axilite port=cols             bundle=control
+      #pragma HLS INTERFACE s_axilite port=sigma            bundle=control
 
-      int img_size = rows * cols;
+      #pragma HLS INTERFACE s_axilite port=o_rows           bundle=control
+      #pragma HLS INTERFACE s_axilite port=o_cols           bundle=control
 
-      xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPC1> img_a(rows, cols);
-      xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPC1> img_c(rows, cols);
+      #pragma HLS INTERFACE s_axilite port=return           bundle=control
 
-      #pragma HLS stream variable=img_a.data depth=1
-      #pragma HLS stream variable=img_c.data depth=1
+      #pragma HLS inline off
+      #pragma HLS dataflow
 
-      unsigned int *dst_a = (unsigned int*)img_a.data;
+      const int pROWS = HEIGHT;
+      const int pCOLS = WIDTH;
+      const int pNPC1 = NPC1;
 
-      read_data_a: for (int i = 0 ; i < img_size/4; i++)
-        {
-          #pragma HLS LOOP_TRIPCOUNT min=1 max=65536
+	    hls::stream<XF_TNAME(SRC_T,NPC1)> src;
+	    hls::stream<XF_TNAME(SRC_T,NPC1)> flt;
+	    hls::stream<XF_TNAME(SRC_T,NPC1)> dst;
 
-          unsigned int ta = a[i];
+	    /********************************************************/
 
-          dst_a[i] = ta;
-        }
+	    Read_yuyv_Loop:
+	    for(int i=0; i < rows; i++)
+	      {
+	        #pragma HLS LOOP_TRIPCOUNT min=1 max=pROWS
 
-      xf::GaussianBlur<FILTER_WIDTH, XF_BORDER_CONSTANT, XF_8UC1, HEIGHT, WIDTH, NPC1>(img_a, img_c, sigma);
+		      for(int j=0; j < (cols)>>(XF_BITSHIFT(NPC1));j++)
+		        {
+	            #pragma HLS LOOP_TRIPCOUNT min=1 max=pCOLS/pNPC1
+			        #pragma HLS PIPELINE
+			        #pragma HLS loop_flatten off
 
-      unsigned int *src = (unsigned int *)img_c.data;
+			        src.write( *(ai + i*(cols>>(XF_BITSHIFT(NPC1))) +j) );
+		        }
+	      }
 
-      write_data_c: for (int i = 0 ; i < img_size/4; i++)
-        {
-          #pragma HLS LOOP_TRIPCOUNT min=1 max=65536
+	    xf::xFGaussianFilter< HEIGHT, WIDTH, XF_DEPTH(SRC_T, NPC1), NPC1, XF_WORDWIDTH(SRC_T,NPC1)>(src, flt, FILTER_WIDTH, XF_BORDER_CONSTANT, rows, cols,sigma);
 
-          c[i] = src[i];
-        }
+
+                                                                  //setup same maximum output image size as for source image
+
+	    xf::xFresize< HEIGHT, WIDTH, SRC_T, NPC1, XF_WORDWIDTH(SRC_T,NPC1), HEIGHT, WIDTH>(flt, dst, INTERPOLATION_TYPE, rows, cols, o_rows, o_cols);
+		
+      for(int i=0;i<o_rows;i++)
+	      {
+          #pragma HLS LOOP_TRIPCOUNT min=1 max=pROWS
+		  
+          for(int j=0;j < o_cols>>XF_BITSHIFT(NPC1); j++)
+		        {
+              #pragma HLS LOOP_TRIPCOUNT min=1 max=pCOLS
+              #pragma HLS PIPELINE II=1
+              #pragma HLS LOOP_FLATTEN OFF
+			
+              *(bo + i*(o_cols>>XF_BITSHIFT(NPC1)) + j) = dst.read();
+		        }
+	      }
+
     }
 
 
